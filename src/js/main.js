6534a08b57e92b135b7b9e246c1b0f9e1b35f70c
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2025-08-18 22:33:17 +0200

    feature: overhaul header with mobile menu, icons, and favicon
    - Added logo/title, responsive navbar, hamburger toggle
    - Search and account icons implemented
    - Layout adjustments for sticky header
    - Updated favicons and web manifest
    - Prettier + Tailwind config updates, added hamburger animation

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+const initApp = () => {
+  const hamburgerButton = document.getElementById("hamburger-button");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  const toggleMenu = () => {
+    mobileMenu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("flex");
+    hamburgerButton.classList.toggle('toggle-btn');
+    document.body.classList.toggle('overflow-hidden'); //disables scroll-function inside menu
+  }
+
+  hamburgerButton.addEventListener("click", toggleMenu);
+  mobileMenu.addEventListener("click", toggleMenu);
+
+}
+
+document.addEventListener("DOMContentLoaded", initApp);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html"],
+  darkMode: "media",
+  content: ["./src/*.html", "./src/js/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Bitcount: ["Bitcount Grid Double", "sans-serif"],
+        Roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        lightbg: "#FDFDFD", // Light Mode Hintergrund
+        darkbg: "#1E293B", // Dark Mode Hintergrund
+      },
+      keyframes: {
+        "open-menu": {
+          "0%": { transform: "scaleY(0)" },
+          "80%": { transform: "scaleY(1.2)" },
+          "100%": { transform: "scaleY(1)" },
+        },
+      },
+      animation: {
+        "open-menu": "open-menu 0.5s ease-in-out forwards",
+      },
+    },
   },
   plugins: [],
-}
-
+};
